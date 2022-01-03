430c0d955bbd88d9e50f29a75fd7b27942470d7c
srinath ragalla <srinathragalla17@gmail.com>
2022-01-03 23:49:24 +0530

    api

---
+++ api.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const http=require('http');
+const url = require('url');
+const server=http.createServer((req,res)=>{
+    const pathName=req.url;
+    if(pathName ==='/Home' || pathName ==='/'){
+        res.end('welcome to Homepage')
+    }else if(pathName ==='/contact'){
+        res.end('Here you can find all our contact details contact')
+    }else if(pathName ==='/api'){
+        fs.readFile('./data.json','utf-8',(err,data)=>{
+            const productData=JSON.parse(data);
+            res.writeHead(200,{'content-type':'application/json'})
+            res.end(data);
+        })
+    }
+    
+});
+server.listen(8000,'127.0.0.1',() => {
+    console.log('listening to port at 8000');
+});
